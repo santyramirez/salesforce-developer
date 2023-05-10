087b572150a9a018f2bdc9b60ed323ecb3c0bc7d
Santiago Rammirez <santiramirez.dev@gmail.com>
2023-05-10 12:18:04 +0200

    fixed warning language & added trackDecorator doc

--- force-app/main/default/lwc/trackDecorator/trackDecorator.js
+++ force-app/main/default/lwc/trackDecorator/trackDecorator.js
@@ -12,10 +12,10 @@ export default class TrackDecorator extends LightningElement {
     handleClick() {
         if (!this.newItem) {
             const event = new ShowToastEvent({
-                title: 'Errore',
-                message: 'Inserisci un elemento prima di aggiungerlo alla lista',
+                title: 'Error:',
+                message: 'Enter an item before adding it to the list.',
                 variant: 'error',
-            });
+            });            
             this.dispatchEvent(event);
         } else {
             this.itemList.push(this.newItem);
@@ -27,17 +27,3 @@ export default class TrackDecorator extends LightningElement {
         this.newItem = '';
     }
 }
-
-// @track: It is used to make variable private but reactive. 
-// Tracked properties are also called private reactive properties.
-// Fields are reactive. If a field’s value changes, and the field is 
-// used in a template or in a getter of a property that’s used in a template, 
-// the component rerenders and displays the new value.
-
-// When a field contains an object or an array, there’s a limit to the depth
-// of changes that are tracked. To tell the framework to observe changes to the 
-// properties of an object or to the elements of an array, decorate the field with @track.
-
-// Note: we can't access @track properties from outside as they are private and only
-// accessible within its component only. To use @track we have to import it first from lwc.
-// Import @track decorator from lwc
